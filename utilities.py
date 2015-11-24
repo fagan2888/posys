582f390cf68d4fa3a1674d4ac9dfca6fc0a1bd5e
sarangof <sarangofc@gmail.com>
2015-11-24 11:35:19 -0500

    Fist attempt to set up the coefficients right

--- utilities.py
+++ utilities.py
@@ -5,32 +5,50 @@ Created on Mon Oct 26 17:47:04 2015
 @author: Sara Arango and Gregory Dobler
 """
 import numpy as np
+from ybus import ybus
+from network import get_network
+
 # Sample input for a very simple network
 
-def get_params():
+def get_params(setting):
     """
     Return the parameters for the system.
     """
-    # GGD: what do these variables stand for?
-    # GGD: also change the names to something more readable
-    GG     = np.array([[4,0,-4],
+    if setting == '3 bus': 
+            GG     = np.array([[4,0,-4],
                        [0,4,-4],
                        [-4,-4,8]])
-    BB     = np.array([[-5,0,5], 
+            BB     = np.array([[-5,0,5], 
                        [0,-10,10],
                        [5,10,-15]])
+            #Q_d    = np.array([0, 0, -0.9397])
+            #P_d    = np.array([0, 2.6860,-2.9313])
+            N      = 3
+            NG     = 2
+            NL     = 1
+            vm     = np.array([1.0,1.1249,0.93834])
+            an     = np.array([0.0, 6.3, -3.44])
+            e      = vm*np.cos(an*np.pi/180.)
+            f      = vm*np.sin(an*np.pi/180.)
+            pgspec = np.array([1.7])
+            plspec = np.array([-2])
+            qlspec = np.array([-1])
+            
+    elif setting == '14 bus':
+            bus,line = get_network()
+            BB, GG = ybus(bus,line)            
+            NG = sum(bus[:,8]==2)
+            NL = sum(bus[:,8]==3)
+            pgspec = bus[bus[:,8]==2,3]
+            plspec = bus[bus[:,8]==3,5]
+            qlspec = bus[bus[:,8]==3,6]
+            # vgspec
+            N = NG - 1 + 2*NL
+            vm = bus[bus[:,8]!=3,1]
+            an = bus[bus[:,8]!=3,2]
+            e  = vm*np.cos(an*np.pi/180.)
+            f  = vm*np.sin(an*np.pi/180.)
 
-    Q_d    = np.array([0, 0, -0.9397])
-    P_d    = np.array([0, 2.6860,-2.9313])
-    N      = 3
-    NG     = 2
-    NL     = 1
-    vm     = np.array([1.0,1.1249,0.93834])
-    an     = np.array([0.0, 6.3, -3.44])
-    e      = vm*np.cos(an*np.pi/180.)
-    f      = vm*np.sin(an*np.pi/180.)
-    pgspec = np.array([1.7])
-    plspec = np.array([-2])
-    qlspec = np.array([-1])
-
-    return [GG, BB, Q_d, P_d, N, NG, NL, vm, an, e, f, pgspec, plspec, qlspec]
+    return [GG, BB, N, NG, NL, vm, an, e, f, pgspec, plspec, qlspec]
+    # GGD: what do these variables stand for?
+    # GGD: also change the names to something more readable
