0e986dfbf1e7e2cbf8075ef62f192768cee0fa82
sarangof <sarangofc@gmail.com>
2015-11-28 13:03:58 -0500

    With more general indexing for loads and generators anywhere in the input matrices

--- power.py
+++ power.py
@@ -6,7 +6,7 @@ Created on Tue Nov 10 09:58:16 2015
 """
 import numpy as np
 
-def power_uo(GG,BB,N,NG,NL,e,f,pgspec,plspec,qlspec):
+def power_uo(GG,BB,N,NG,NL,e,f,pgspec,plspec,qlspec,ind_gen,ind_load):
     """
     Calculate the real and reactive power (P and Q) as well as Delta P and 
     Delta Q
@@ -15,15 +15,16 @@ def power_uo(GG,BB,N,NG,NL,e,f,pgspec,plspec,qlspec):
     # -- calculate P and Q
     a_sum = (GG*e - BB*f).sum(1)
     b_sum = (GG*f + BB*e).sum(1)
+    
     P     = e*a_sum + f*b_sum
     Q     = f*a_sum - e*b_sum
     
     # -- calculate the Deltas
-    pb = np.concatenate([pgspec[:NG-1] - P[1:NG],  
-                         plspec[:NL] - P[NG:NG+NL],
-                         qlspec[:NL] - Q[NG:NG+NL]])
+    dtv   = np.concatenate([pgspec[:NG-1] - P[ind_gen],  
+                         plspec[:NL] - P[ind_load],
+                         qlspec[:NL] - Q[ind_load]])
                          
     #np.vstack((np.hstack((HH[1:2*NG-1,1:2*NG-1],NN[1:2*NG-1,NG:N])),np.hstack((JJ[NG:N,1:2*NG-1], LL[NG:N,NG:N] ))))
 
-    return P, Q, pb
+    return P, Q, dtv
    
