e586e8b2f0dd3edcc34fde457592dd5602627eed
Sarangof <sarah.arango@gmail.com>
2016-02-10 12:33:02 -0500

    Adding minimal visualization possibilities

--- outvariance.py
+++ outvariance.py
@@ -4,16 +4,14 @@ Created on Wed Feb 10 10:37:31 2016
 
 Using PYPOWER to estimate the impact of small variations in the 
 obtained angles and voltages.
-
-@author: saf537
 """
-
-
 
 import pypower.api as pypo # to install, just pip install PYPOWER
 import case14_mod
 from numpy import zeros, hstack
-#import pandas as pd
+import pandas as pd
+from pylab import imshow
+#import matplotlib as matplot
 
 dlt_vec = [0.05, -0.05, 0.10, -0.10, 0.50, -0.50]
 mtr     = zeros([9,22,len(dlt_vec)]) 
@@ -27,7 +25,14 @@ for j in range(0,len(dlt_vec)):
         volt     = r[0]['bus'][r[0]['bus'][:,1] == 1,7]  # voltage 7, type 2 
         ang      = r[0]['bus'][r[0]['bus'][:,1] != 3,8]  # angle 8, type 2
         mtr[i,:,j] = hstack((volt,ang))
+        
 
+for j in range(0,len(dlt_vec)):
+    print "Matrix", format(100*dlt_vec[j]), ' %'
+    print ' ' 
+    imshow(mtr[:,:8,j])
+    imshow(mtr[:,8:,j])
+    print pd.DataFrame(mtr[:,:,j])
 
 # 
 #pypo.printpf(r)
