45f1eb8d32ee927a37bdfd3bf6ecdd6956015a87
Sara Arango <sarangofc@gmail.com>
2016-03-01 17:31:15 -0500

    results

--- posys/outvariance.py
+++ posys/outvariance.py
@@ -30,13 +30,13 @@ ppc0      = case14_mod.case14_mod(busN = 1,dlt = 0) # trivial case: original sol
 #                          OUT_V_LIM    = False) # Careful: have to use Newton Method!!!
 ppopt0    = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) # Careful: have to use Newton Method!!!
 r0        = pypo.runpf(ppc0, ppopt0)       
-m1        = r0[0]['bus'][r0[0]['bus'][:,1] == 2,2]
+m1        = r0[0]['gen'][:,1]
 
 # CORRECT THIS!!!
-p0        = [m1 for i in range(0,4)] 
-p0        = transpose(p0)
+p0        = [m1 for i in range(0,8)] 
+#p0        = transpose(p0)
 #a0        = transpose(a0)
-mtr       = zeros([8,4,len(dlt_vec)]) 
+mtr       = zeros([8,5,len(dlt_vec)]) 
 
 j = 1
 for j in range(0,len(dlt_vec)):
@@ -48,8 +48,7 @@ for j in range(0,len(dlt_vec)):
             ppopt        = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) # Careful: have to use Newton Method!!!
             r            = pypo.runpf(ppc, ppopt)
             #plt.plot(r[0]['bus'][:,2])
-            pwr          = r[0]['bus'][r[0]['bus'][:,1] == 2,2]  # voltage 7, type 2 
-            print r[0]['bus'][r[0]['bus'][:,1] == 1,2]
+            pwr          = r[0]['gen'][:,1] # voltage 7, type 2 
             mtr[tmp,:,j] = pwr
             del(ppc, ppopt, r, pwr)
             tmp += 1
@@ -57,24 +56,24 @@ for j in range(0,len(dlt_vec)):
         
 
 ## -- plot results
-#figs = []
-#axs = []
-#ims = []
-#
-#for j in range(0,len(dlt_vec)):
-#    figs.append(plt.figure(figsize=(10,15)))
-#    axs.append(figs[j].add_subplot(1,1,1))
-#    # CORRECT THIS!
-#    ims.append(axs[j].imshow(100*(mtr[:,:,j]-p0) / p0,
-#                              interpolation="nearest",
-#                               cmap="seismic"))
-#    axs[j].set_title('Powers (Power change: {0})'.format(dlt_vec[j]))
-#    vname   = 'Power' + str(dlt_vec[j]) + '.png'
-#    figs[j].colorbar(ims[j],orientation ='horizontal')
-#    axs[j].set_xlabel('Number of transfomers - generators')
-#    axs[j].set_ylabel('Load that changed (buildings)')
-#    figs[j].savefig(os.path.join("../output",vname), dpi=100, clobber=True)
-#    
+figs = []
+axs = []
+ims = []
+
+for j in range(0,len(dlt_vec)):
+    figs.append(plt.figure(figsize=(10,15)))
+    axs.append(figs[j].add_subplot(1,1,1))
+    # CORRECT THIS!
+    ims.append(axs[j].imshow(100*(mtr[:,:,j]-p0) / p0,
+                              interpolation="nearest",
+                               cmap="seismic"))
+    axs[j].set_title('Powers (Power change: {0})'.format(dlt_vec[j]))
+    vname   = 'Power' + str(dlt_vec[j]) + '.png'
+    figs[j].colorbar(ims[j],orientation ='horizontal')
+    axs[j].set_xlabel('Number of transfomers - generators')
+    axs[j].set_ylabel('Load that changed (buildings)')
+    figs[j].savefig(os.path.join("../output",vname), dpi=100, clobber=True)
+    
             
 
   
