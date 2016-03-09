f8997c78f23263349fea3eb7d17a79bf96ad7ae2
Sara Arango <sarangofc@gmail.com>
2016-03-09 08:36:35 -0500

    Getting the right power measurements

--- posys/outvariance.py
+++ posys/outvariance.py
@@ -30,9 +30,9 @@ ppc0      = case14_mod.case14_mod(busN = 1,dlt = 0) # trivial case: original sol
 #                          OUT_V_LIM    = False) # Careful: have to use Newton Method!!!
 ppopt0    = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) # Careful: have to use Newton Method!!!
 r0        = pypo.runpf(ppc0, ppopt0)       
-m1        = r0[0]['gen'][:,1]
+m1        = r0[0]['gen'][:,2][1:]
 p0        = [m1 for i in range(0,8)] 
-mtr       = zeros([8,5,len(dlt_vec)]) 
+mtr       = zeros([8,4,len(dlt_vec)]) 
 
 j = 1
 for j in range(0,len(dlt_vec)):
@@ -43,7 +43,7 @@ for j in range(0,len(dlt_vec)):
             ppopt        = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) # Careful: have to use Newton Method!!!
             r            = pypo.runpf(ppc, ppopt)
             #plt.plot(r[0]['bus'][:,2])
-            pwr          = r[0]['gen'][:,1] # voltage 7, type 2 
+            pwr          = r[0]['gen'][:,2][1:] # voltage 7, type 2 
             mtr[tmp,:,j] = pwr
             del(ppc, ppopt, r, pwr)
             tmp += 1
