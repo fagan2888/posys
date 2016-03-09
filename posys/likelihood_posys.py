d657c9716f5ca009295299f6689d16db9d92ccae
Sara Arango <sarangofc@gmail.com>
2016-03-09 09:43:37 -0500

    Fixing return function

--- posys/likelihood_posys.py
+++ posys/likelihood_posys.py
@@ -5,12 +5,14 @@ Created on Wed Mar  9 09:26:19 2016
 @author: saf537
 """
 import numpy as np
+import case14_mod
+import pypower.api as pypo 
 
 def likelihood_ps(bus_vec,measur_vec):      
-    ppc     = case14_mod.case14_mod(busN = i,dlt = dlt_vec[j])
+    ppc     = case14_mod.case14_mod(busN = 0, dlt = 0, op_change=2, invec = measur_vec)
     ppopt   = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) # Careful: have to use Newton Method!!!
     r       = pypo.runpf(ppc, ppopt)
-    estim   =
-    sig     =  
-    return np.exp(()**2/sig**2)        
+    estim   = r[0]['gen'][:,2][1:]  
+    sig     = np.std(estim - measur_vec)
+    return np.exp((estim - measur_vec)**2/sig**2)        
     
