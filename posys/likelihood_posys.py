7fbc18b2897618f92a6249cbb0bbd449382ed7fb
Greg Dobler <greg.dobler@gmail.com>
2016-03-23 12:55:28 -0400

    don't overwrite ppc0

--- posys/likelihood_posys.py
+++ posys/likelihood_posys.py
@@ -5,9 +5,11 @@ Created on Wed Mar  9 09:26:19 2016
 @author: saf537
 """
 import numpy as np
+import copy as cp
 import pypower.api as pypo 
 from get_ppc14 import *
 import matplotlib.pyplot as plt
+import pdb
 
 #def likelihood_ps(bus_vec,measur_vec):      
 def likelihood_ps(measur_vec,bvec):
@@ -22,28 +24,36 @@ def likelihood_ps(measur_vec,bvec):
     estim   = r[0]['gen'][:,2] 
     
     # -- calculate the likelihood
-    sig     = np.std(estim - measur_vec)
-    return np.exp(((estim - measur_vec)**2).sum()/sig**2)        
+#    sig     = np.std(estim - measur_vec)
+    sig     = 10.0
+#    pdb.set_trace()
+    return np.exp(-((estim - measur_vec)**2).sum()/(2*sig**2))
         
 ppc0       = get_ppc14(op_change=1,dlt=0,busN=1) #trivial case: original solutin
+ppc = cp.deepcopy(ppc0)
 measur_vec = ppc0['gen'][:,2]
 ind   = ppc0["bus"][:,1]==1 # 1 is load bus
 np.random.seed(314)
 
 # -- modify the load buses
 
-#loads = ppc0['bus'][:,0][ppc0['bus'][:,1]==1]
+loads = ppc0['bus'][:,0][ppc0['bus'][:,1]==1]
 tmp = 0
 LK_vec = []
 for bs in loads:
 #    L          = likelihood_ps(measur_vec,bvec)
     #bvec  = ppc0["bus"][ind,2] + 0.5*np.random.randn(ind.sum())
     L_vec = []
-    for mod in np.arange(0,1,0.05):
-        bvec  = ppc0["bus"][ind,2]
-        bvec[tmp] =  mod*np.random.randn()
+    for mod in np.arange(0,2.1,0.1):
+        bvec  = ppc0["bus"][ind,2].copy()
+#        bvec[tmp] =  mod*np.random.randn()
+#        pdb.set_trace()
+        bvec[tmp] *=  mod
+
         bvec *= bvec>0.0
+
         L     = likelihood_ps(measur_vec,bvec)
+
         L_vec.append(L)
     plt.plot(L_vec)
     LK_vec.append(L_vec)
