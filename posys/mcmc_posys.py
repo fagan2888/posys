8503992a32e54c85825d50337e07639e91ac0478
Sara Arango <sarangofc@gmail.com>
2016-04-08 00:00:26 -0400

    Looping through initial loads

--- posys/mcmc_posys.py
+++ posys/mcmc_posys.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Urban Observatory
-
 """
 
 import numpy as np
@@ -13,7 +12,8 @@ import emcee
 import corner
 
 #ndim, nwalkers = 9, 300
-ndim, nwalkers = 9, 30
+ndim, nwalkers = 9, 50
+nsteps = 100
 
 def likelihood_ps(theta,y):
     # -- modify load buses
@@ -38,18 +38,23 @@ np.random.seed(314)
 theta  = ppc0["bus"][ind,2].copy()#+ 1e-0*np.random.randn(ndim)
 theta *= theta>0.0
 
-pos = [theta + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
-
-print("initializing sampler...")
-sampler = emcee.EnsembleSampler(nwalkers, ndim, likelihood_ps, args=[y])
-
-print("running walkers...")
-#sampler.run_mcmc(pos, 500)
-sampler.run_mcmc(pos, 500)
-print("walkers finished...")
-
-#samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
-samples = sampler.chain[:, :, :].reshape((-1, ndim))
+for val in [1.00, 2.00, 5.00, 10.00, 50.00]:
+    #pos = [theta + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+    pos = [val*np.ones(ndim) for i in range(nwalkers)]    
+    
+    print("initializing sampler...")
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, likelihood_ps, args=[y])
+    
+    print("running walkers...")
+    #sampler.run_mcmc(pos, 500)
+    sampler.run_mcmc(pos, nsteps)
+    print("walkers finished...")
+    
+    #samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+    #samples = sampler.chain[:, :, :].reshape((-1, ndim))
+    nm = '%sWalk%s9build%ssteps%sval'%(nwalkers,ndim,nsteps,val)
+    np.save(nm, sampler.chain)
+    
 
 #fig = corner.corner(samples, labels=["$b1$", "$b2$", "$b3$","$b4$","$b5$",
 #                                     "$b6$","$b7$","$b8$","$b9$"],
@@ -60,7 +65,6 @@ samples = sampler.chain[:, :, :].reshape((-1, ndim))
 m_true = -0.9594
 b_true = 4.294
 f_true = 0.534
-
 # Generate some synthetic data from the model.
 N = 50
 x = np.sort(10*np.random.rand(N))
@@ -68,26 +72,19 @@ yerr = 0.1+0.5*np.random.rand(N)
 y = m_true*x+b_true
 y += np.abs(f_true*y) * np.random.randn(N)
 y += yerr * np.random.randn(N)
-
-
 A = np.vstack((np.ones_like(x), x)).T
 C = np.diag(yerr * yerr)
 cov = np.linalg.inv(np.dot(A.T, np.linalg.solve(C, A)))
 b_ls, m_ls = np.dot(cov, np.dot(A.T, np.linalg.solve(C, y)))
-
 def lnlike(theta, x, y, yerr):
     m, b, lnf = theta
     model = m * x + b
     inv_sigma2 = 1.0/(yerr**2 + model**2*np.exp(2*lnf))
     return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
-
-
 import scipy.optimize as op
 nll = lambda *args: -lnlike(*args)
 result = op.minimize(nll, [m_true, b_true, np.log(f_true)], args=(x, y, yerr))
 m_ml, b_ml, lnf_ml = result["x"]
-
-
 def lnprior(theta):
     m, b, lnf = theta
     if -5.0 < m < 0.5 and 0.0 < b < 10.0 and -10.0 < lnf < 1.0:
@@ -102,20 +99,15 @@ def lnprob(theta, x, y, yerr):
     
 ndim, nwalkers = 3, 100
 pos = [result["x"] + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
-
     
 import emcee
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(x, y, yerr))
-
 sampler.run_mcmc(pos, 500)
-
 samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
-
 import corner
 fig = corner.corner(samples, labels=["$m$", "$b$", "$\ln\,f$"],
                       truths=[m_true, b_true, np.log(f_true)])
 fig.savefig("triangle.png")
-
 samples[:, 2] = np.exp(samples[:, 2])
 m_mcmc, b_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                              zip(*np.percentile(samples, [16, 50, 84],
