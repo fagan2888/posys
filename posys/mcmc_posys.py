21f64985e45bd90712ec2c2b9bc25bf676ebaf41
Sara Arango <sarangofc@gmail.com>
2016-04-20 12:10:51 -0400

    Random initializations.

--- posys/mcmc_posys.py
+++ posys/mcmc_posys.py
@@ -12,16 +12,22 @@ import emcee
 import corner
 
 #ndim, nwalkers = 9, 300
-ndim, nwalkers = 9, 50
-nsteps = 200
+ndim, nwalkers = 9, 300
+nsteps = 500
+cut = 50
+
+# Refer to https://github.com/rwl/PYPOWER/blob/master/pypower/newtonpf.py
 
 def likelihood_ps(theta,y):
+    
     # -- modify load buses
     ppc["bus"][ind,2] = theta
+    
     # -- estimate the transformer measurements
     ppopt   = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) 
     r       = pypo.runpf(ppc, ppopt)
     estim   = r[0]['gen'][:,2] 
+    
     # -- calculate the likelihood
     sig     = 10.0
     if (theta>=0.0).all():
@@ -32,37 +38,49 @@ def likelihood_ps(theta,y):
 # Choose the "true" parameters.
 ppc0       = get_ppc14(op_change=1,dlt=0,busN=1) #trivial case: original solutin
 ppc = cp.deepcopy(ppc0)
-y = ppc0['gen'][:,2].copy()
+y = ppc0['gen'][:,2].copy() # Measured values in the transformers
 ind   = ppc0["bus"][:,1]==1 
 theta = []
 
-for val in [1.00, 2.00, 5.00, 10.00, 50.00]:
 
-    np.random.seed(314)
-    del(theta)
-    theta  = [val*np.ones(ndim)]#ppc0["bus"][ind,2].copy()#+ 1e-0*np.random.randn(ndim)
-    theta *= theta>0.0
-
-    #pos = [theta + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
-    pos = [val*np.ones(ndim) for i in range(nwalkers)]    
+for nit in range(0,5):
+    for val in [20.00, 50.00]:
+        # -- Inputting loads
+        np.random.seed(314)
+        del(theta)
+        theta  = [val*np.ones(ndim)]#ppc0["bus"][ind,2].copy()#+ 1e-0*np.random.randn(ndim)
+        theta *= theta>0.0
     
-    print("initializing sampler...")
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, likelihood_ps, args=[y])
-    
-    print("running walkers...")
-    #sampler.run_mcmc(pos, 500)
-    sampler.run_mcmc(pos, nsteps)
-    print("walkers finished...")
-    
-    #samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
-    #samples = sampler.chain[:, :, :].reshape((-1, ndim))
-    nm = '%sWalk%sbuild%ssteps%sval'%(nwalkers,ndim,nsteps,val)
-    np.save(nm, sampler.chain)
+        # -- Initializing mcmc walkers
+        #pos = [theta + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+        #pos = [np.abs(val*np.ones(ndim)+ 2e+1*np.random.randn(ndim))  for i in range(nwalkers)]    
+        pos = np.random.uniform(0,100,size=[nwalkers,ndim])       
+        
+        print("initializing sampler...")
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, likelihood_ps, args=[y])
+        
+        print("running walkers...")
+        #sampler.run_mcmc(pos, 500)
+        sampler.run_mcmc(pos, nsteps)
+        print("walkers finished...")
+        
+        #samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
+        #samples = sampler.chain[:, :, :].reshape((-1, ndim))
+        nm = '%sWalk%sbuild%ssteps%svalRandomInit%s'%(nwalkers,ndim,nsteps,val,nit)
+        np.save('random/'+nm, sampler.chain)
+        
+        np.save('random/median_'+nm,np.median(np.median(sampler.chain[:,cut:,:],axis=0),axis=0) - theta)
     
 
 #fig = corner.corner(samples, labels=["$b1$", "$b2$", "$b3$","$b4$","$b5$",
 #                                     "$b6$","$b7$","$b8$","$b9$"],
 #                    truths=theta)
+#                    
+
+
+#for fl in Walk9build500steps1,Walk9build500steps2, Walk9build500steps50,Walk9build500steps5:
+    
+    
 
 
 """
