4746e0df11e79f215559f35101bef1dddff39b01
Sara Arango <sarangofc@gmail.com>
2016-05-04 10:51:42 -0400

    Testing around other random variables

--- posys/mcmc_posys.py
+++ posys/mcmc_posys.py
@@ -18,9 +18,8 @@ def lnlike(theta,y):
     # -- modify load buses
     ppc["bus"][ind,2] = theta
     
-    # -- estimate the transformer measurements
-    ppopt = pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0) 
-    sol   = pypo.runpf(ppc, ppopt)
+    # -- estimate the transformer measurements 
+    sol   = pypo.runpf(ppc, pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0))
     estim = sol[0]['gen'][:,2] 
     
     # -- calculate the likelihood
@@ -30,13 +29,11 @@ def lnlike(theta,y):
     else:
         return -np.inf
 
-
 # -- utilities
 ndim     = 9
 nwalkers = 30
 nsteps   = 100
-cut      = 50
-
+cut      = 5
 
 # -- intialize the 14-bus system
 ppc0  = get_ppc14(1,0,1) # 0 implies no change
@@ -45,22 +42,22 @@ y     = ppc0['gen'][:,2].copy() # default measured values of transformers
 ind   = ppc0["bus"][:,1]==1 # building indices
 binit = ppc0["bus"][ind,2].copy()
 
-
-# -- Initialize sampler
-print("initializing sampler...")
-np.random.seed(314)
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args=[y])
-pos     = np.array([binit*(1.0+0.2*np.random.randn(ndim)) for i in 
-                    range(nwalkers)]).clip(min=0.0)
-
-
-# -- run walkers
-print("running walkers...")
-sampler.run_mcmc(pos, nsteps)
-print("walkers finished...")
-
-
-# -- save chain
-#oname = "../output/random_test3.npy"
-#print("saving chain to {0}".format(oname))
-#np.save(oname,sampler.chain)
+for val in [1.00, 2.00, 5.00, 10.00, 20.00]:
+    # -- Initialize sampler
+    print("initializing sampler...")
+    np.random.seed(314)
+    y = val*np.ones(len(y))
+    binit = val*np.ones(ndim) 
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args=[y])
+    pos     = np.array([binit*(1.0+0.2*np.random.randn(ndim)) for i in 
+                        range(nwalkers)]).clip(min=0.0)
+        
+    # -- run walkers
+    print("running walkers...")
+    sampler.run_mcmc(pos, nsteps)
+    print("walkers finished...")
+    
+    # -- save chain
+    oname = "../output/random_test20percent"+str(val)+".npy"
+    print("saving chain to {0}".format(oname))
+    np.save(oname,sampler.chain)
