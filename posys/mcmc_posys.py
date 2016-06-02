565214c86f34dd4bec57be693dc654e015650040
Arango-Franco Sara-maria <saf537@compute.cusp.nyu.edu>
2016-06-02 15:54:23 -0400

    Test for different building values.

--- posys/mcmc_posys.py
+++ posys/mcmc_posys.py
@@ -3,7 +3,7 @@
 # Urban Observatory
 
 import numpy as np
-import copy as cp
+import copy as cp 
 import pypower.api as pypo 
 from get_ppc14 import *
 import matplotlib.pyplot as plt
@@ -31,25 +31,24 @@ def lnlike(theta,y):
 # -- utilities
 ndim     = 9
 nwalkers = 30
-nsteps   = 200
+nsteps   = 80
 cut      = 5
 
 # -- intialize the 14-bus system
 ppc0  = get_ppc14(1,0,1) # 0 implies no change
 ppc   = cp.deepcopy(ppc0)
-y     = ppc0['gen'][:,2].copy() # default measured values of transformers
+#y     = ppc0['gen'][:,2].copy() # default measured values of transformers
 ind   = ppc0["bus"][:,1]==1 # building indices
-binit = ppc0["bus"][ind,2].copy()
+#binit = ppc0["bus"][ind,2].copy()
 
 for val in [1.00, 2.00, 5.00, 10.00, 20.00]:
-	# -- Initialize sampler
+	# -- Initialize sample
 	print("initializing sampler...")
 	np.random.seed(314)
-	y = val*np.ones(len(y))
-	sol   = pypo.runpf(ppc, pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0))
-	estim = sol[0]['bus'][ind,2]
-
-	binit = val*np.ones(ndim) 
+	ppc["bus"][ind,2] = val*np.ones(ndim)
+	sol     = pypo.runpf(ppc, pypo.ppoption(PF_ALG=2, VERBOSE=0, OUT_ALL=0))
+	y       = sol[0]['gen'][:,2]
+	binit   = ppc['bus'][ind,2].copy()	
 	sampler = emcee.EnsembleSampler(nwalkers, ndim, lnlike, args=[y])
 	pos     = np.array([binit*(1.0+0.2*np.random.randn(ndim)) for i in range(nwalkers)]).clip(min=0.0)
 	# -- run walkers
@@ -58,6 +57,6 @@ for val in [1.00, 2.00, 5.00, 10.00, 20.00]:
 	print("walkers finished...")
 
 	# -- save chain
-	oname = "../output/random_test20percent"+str(val)+".npy"
+	oname = "./output/random_test20percent_diffValues"+str(val)+".npy"
 	print("saving chain to {0}".format(oname))
 	np.save(oname,sampler.chain)
